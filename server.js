90868d82cb81348ff6cf019aca21658c0edf5973
Bing Z <bing@bings-mbp.lan>
2020-10-24 23:09:54 -0600

    connected to server

---
+++ server.js
@@ -0,0 +1,31 @@
+//Import packages and modules
+const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
+const db = require("./models");
+
+
+//Setup PORT
+const PORT = process.env.PORT || 8080;
+
+//Create express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Use Session to keep track of user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//requiring routes
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+//Syncing databse and loggin a message to the user upon successully connection to server 
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
+});
