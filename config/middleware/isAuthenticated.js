9b3c99155626c72263de537a3cb6007cbc909fae
Bing Z <bing@bings-mbp.lan>
2020-10-25 20:41:06 -0600

    coreected typo exports

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,6 +1,6 @@
 //A Middleware for restricting routes a user is not allowed to visit if not logged in
 
-module.export = (req, res, next) => {
+module.exports = (req, res, next) => {
     //If a user is already logged in, then continue with the request to the restricted route
     if (req.user) {
         return next();
